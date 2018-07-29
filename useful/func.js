767705def08b5e0f81d30ae5413ad4ec284ced60
Artem Tarasov <artemtarasov89@gmail.com>
2018-07-29 15:42:29 -0400

    general cleanup

--- useful/func.js
+++
@@ -1,6 +0,0 @@
-//sender side
-chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
-    alert(response);
-});
-//reciving side
-chrome.runtime.sendMessage("Send from background")
