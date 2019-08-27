233135cdadd01ec220588332afb362df1bb1d1f9
artemtar <artemtarasov89@gmail.com>
2019-08-27 18:28:36 -0400

    update

--- textcheck/js.js
+++ textcheck/js.js
@@ -1,21 +1,38 @@
 tokenizer = new natural.WordTokenizer();
+// target = ["user", "have", 'jump'];
+target = [];
 
 var analizeText = function(text, toMatch){
-    // var splited = text.split(" ")
-    //                         .filter(word => word.length > 1 && word.length < 20)
-    //                         .map(word => word.trim());
     var tokens = tokenizer.tokenize(text)
-    var clean = tokens
-                    .filter(word => word.length > 1 && word.length < 20)
-                    .map(word => word.removeStopWords());
-    console.log(clean);
-    out = 0;
-    // splited.forEach(function(e){
-    //     if (toMatch.includes(nlp(e).normalize().out('text'))){
-    //         out += 1;
-    //     }
-    // })
-    return out;
+    var cleanWords = tokens
+                    .map(word => word.toLowerCase())
+                    // .map(word => word.removeStopWords())
+                    .filter(word => word.length > 2 && word.length < 20);
+    var uniqWords = [...new Set(cleanWords)];
+    var createDictToAnalize = function(target, words){
+      toAnalize = []
+      target.forEach(function(t){
+          words.forEach(function(word){
+            if (word[0] == t[0] && word[1] == t[1]){
+              toAnalize.push(word);
+            }
+         })
+      }) 
+      return toAnalize;
+    }
+    wordsToCheck = nlp(createDictToAnalize(toMatch, uniqWords))
+                                        .normalize()
+                                        .out('text')
+                                        .split(' ');
+    targetWords = nlp(toMatch)
+                        .normalize()
+                        .out('text')
+                        .split(' ')
+
+    const intersection = wordsToCheck.filter(element => targetWords.includes(element))
+                                     .filter(n => n);
+    console.log(intersection + "intersection")
+    return intersection.length;
     // foundOcurences = nlp(splited)
     //                         .nouns()
     //                         .normalize()
@@ -23,14 +40,6 @@ var analizeText = function(text, toMatch){
     //                         .length;
     // return foundOcurences;
 }
-
-console.log("helloooooooooooooo");
-var text = $('body').text();
-toMatch = ["user", "have"];
-count = analizeText(text, toMatch);
-console.log(count + " matchedc")
-var result = tokenizer.tokenize(text)
-console.log(result);
 // var doc = nlp(text);
 // var nouns = doc
 //                 // .normalize()
@@ -44,29 +53,6 @@ console.log(result);
 //     if(this.src){images.push(this);}
 // });
 // console.log(images)
-checkIfImg = function(toCheck){
-    // console.log(toCheck);
-
-    var backImg;
-    if (toCheck.is('img')) {
-        toCheck.addClass("blur");
-    }
-
-    else {
-        backImg = toCheck.css('background-image');
-        if (backImg != 'none'){
-        console.log("back");
-        toCheck.addClass("blur")
-        }
-    }
-}
-
-var imgs = []
-$('*').each(function(){ 
-    // console.log("hello");
-    // console.log($(this))
-    checkIfImg($(this));
-})
 // function collectTextNodes(element, texts) {
 //     for (var child = element.firstChild; child!==null; child= child.nextSibling) {
 //         if (child.nodeType===3)
@@ -123,21 +109,51 @@ $('*').each(function(){
 //     if (e){
 //     checkIfImg($(e));
 // }}
+
+
+
+
+// checkIfImg = function(toCheck){
+//     // console.log(toCheck);
+
+//     var backImg;
+//     if (toCheck.is('img')) {
+//         toCheck.addClass("blur");
+//     }
+//     else {
+//         backImg = toCheck.css('background-image');
+//         if (backImg != 'none'){
+//         console.log("back");
+//         toCheck.addClass("blur")
+//         }
+//     }
+// }
+
+
+
+
+// var imgs = []
+// $('*').each(function(){ 
+//     // console.log("hello");
+//     // console.log($(this))
+//     checkIfImg($(this));
+// })
+
 //     var observer = new MutationObserver(function (mutations) {
 //         mutations.forEach(function (mutation) {
 //             switch (mutation.type) {
 //             case 'childList':
 //                 Array.prototype.forEach.call(mutation.target.children, function (child) {
-//                     console.log("why");
 //                      if ( child.tagName === "IMG" ) {
 //                          console.log("img");
 //                      }
-//                     child.addEventListener( 'load', c, false );
-//                     // console.log(child);
-//                     // checkIfImg($(child));
+//                     child.addEventListener( 'load', checkIfImg, false );
+//                     console.log("forEachChild");
+//                     console.log(child);
+//                     checkIfImg($(child));
 //                     $(child).each(function(){ 
-//                         console.log("helloha");
-//                         console.log($(this))
+//                         console.log("inside each");
+//                         console.log($(this));
 //                         if ($(this).tagName == "IMG"){
 //                             console.log("img");
 //                         }
@@ -150,5 +166,75 @@ $('*').each(function(){
 //             }
 //         });
 //     });
-// observer.observe(document, {childList: true, subtree: true
-//     });
+// observer.observe(document, {childList: true, subtree: true});
+
+
+// }
+//     var backImg;
+//     if (toCheck.is('img')) {
+//         toCheck.addClass("blur");
+//     }
+//     else {
+//         backImg = toCheck.css('background-image');
+//         if (backImg != 'none'){
+//         console.log("back");
+//         toCheck.addClass("blur")
+//         }
+//     }
+// }
+let imageList = [];
+
+function checkIfImg(toCheck) {
+  let images = toCheck.find('img');
+  for(let image of images) {
+    let imageSource = $(image).attr('src');
+    // $(image).addClass("noblur");
+    console.log("what")
+    console.log($(image));
+    $(image).click(function(e) {
+      var code = e.keyCode || e.which;
+      if (code == 90){
+    $(this).addClass("noblur");
+  }
+
+  });
+    if(!imageList.includes(image)) {
+      imageList.push(image);
+      console.log("pushed: " + imageSource)
+      console.log(imageList)
+    }
+  }
+};
+
+checkIfImg($(document));
+
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(mutation => checkIfImg($(mutation.target)));
+});
+observer.observe(document, { childList: true, subtree: true });
+
+var text = $('body').text();
+count = analizeText(text, target);
+console.log("count " + count);
+if (count == 0){
+  imageList.forEach(function(element){
+    console.log("inside");
+    // console.log(element);
+// element.addClass("noblur")
+  });
+}
+console.log(count + " matchedc")
+
+
+// content script
+var lastElementContext;
+document.addEventListener('contextmenu', function(event) {
+    lastElementContext = event.target;
+}, true);
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  console.log("here")  
+  if (lastElementContext) {
+      console.log("EVENT");
+      $(lastElementContext).addClass("noblur");
+    }
+});
---
+++ textcheck/removeBlur.js
@@ -0,0 +1,5 @@
+	console.log($(e));
+	let img = $(e).find("img");
+	$(e).addClass("noblur");
+	console.log($(img));
+	$(img).addClass("noblur");
